8a34490702a57da51ef6fc9d6295d674e4383c9f
Adam Biser <adambiser@gmail.com>
2017-08-18 15:32:22 -0400

    Events are now just a dict wrapper.
    Easier to create, use, and maintain.
    Removed dead commented code.

--- imfcreator/midi/events.py
+++ imfcreator/midi/events.py
@@ -1,191 +1,139 @@
 from .constants import *
 import struct
 
-
-class MidiEvent:
-    """Represents an abstract event within a MIDI file."""
-    def __init__(self, delta, event_type, data):
-        self.delta = delta
-        self.event_type = event_type
-        self.data = struct.unpack("B" * len(data), data)
-
-    def __repr__(self):
-        return "{}: delta: {}".format(self.__class__.__name__, self.delta)
-
-    @classmethod
-    def create_event(cls, delta, event_type, stream):
-        event_function = MidiEvent._EVENT_FUNC_MAP.get(event_type)
-        if event_function is None:
-            event_function = MidiEvent._EVENT_FUNC_MAP.get(event_type & 0xf0)
-        if event_function is None:
-            raise Exception("Unsupported MIDI event code: 0x{:X}".format((event_type)))
-        return event_function(delta, event_type, stream)
-
-    @classmethod
-    def create_note_off_event(cls, delta, event_type, stream):
-        return NoteOffEvent(delta, event_type, stream.f.read(2))
-
-    @classmethod
-    def create_note_on_event(cls, delta, event_type, stream):
-        return NoteOnEvent(delta, event_type, stream.f.read(2))
-
-    @classmethod
-    def create_polyphonic_key_pressure_event(cls, delta, event_type, stream):
-        return PolyphonicKeyPressureEvent(delta, event_type, stream.f.read(2))
-
-    @classmethod
-    def create_controller_change_event(cls, delta, event_type, stream):
-        return ControllerChangeEvent(delta, event_type, stream.f.read(2))
-
-    @classmethod
-    def create_program_change_event(cls, delta, event_type, stream):
-        return ProgramChangeEvent(delta, event_type, stream.f.read(1))
-
-    @classmethod
-    def create_channel_key_pressure_event(cls, delta, event_type, stream):
-        return ChannelKeyPressureEvent(delta, event_type, stream.f.read(1))
-
-    @classmethod
-    def create_pitch_bend_event(cls, delta, event_type, stream):
-        return PitchBendEvent(delta, event_type, stream.f.read(2))
-
-    @classmethod
-    def create_sysex_event(cls, delta, event_type, stream):
-        data_length = stream._read_var_length()
-        return SysExEvent(delta, event_type, stream.f.read(data_length))
-
-    @classmethod
-    def create_meta_event(cls, delta, event_type, stream):
-        meta_type = stream._next_byte()
-        data_length = stream._read_var_length()
-        return MetaEvent(delta, event_type, meta_type, stream.f.read(data_length))
-
-# Map MIDI event codes to methods used to create the matching event object.
-MidiEvent._EVENT_FUNC_MAP= {
-    NOTE_OFF_EVENT: MidiEvent.create_note_off_event,
-    NOTE_ON_EVENT: MidiEvent.create_note_on_event,
-    POLYPHONIC_KEY_PRESSURE_EVENT: MidiEvent.create_polyphonic_key_pressure_event,
-    CONTROLLER_CHANGE_EVENT: MidiEvent.create_controller_change_event,
-    PROGRAM_CHANGE_EVENT: MidiEvent.create_program_change_event,
-    CHANNEL_KEY_PRESSURE_EVENT: MidiEvent.create_channel_key_pressure_event,
-    PITCH_BEND_EVENT: MidiEvent.create_pitch_bend_event,
-    F0_SYSEX_EVENT: MidiEvent.create_sysex_event,
-    F7_SYSEX_EVENT: MidiEvent.create_sysex_event,
-    META_EVENT: MidiEvent.create_meta_event,
+_META_TYPE_NAMES = {
+    0: 'sequence_number',
+    1: 'text_event',
+    2: 'copyright',
+    3: 'track_name',
+    4: 'instrument_name',
+    5: 'lyric',
+    6: 'marker',
+    7: 'cue_point',
+    0x20: 'channel_prefix',
+    0x2f: 'end_of_track',
+    0x51: 'set_tempo',
+    0x54: 'smtpe_offset',
+    0x58: 'time_signature',
+    0x59: 'key_signature',
+    0x7f: 'sequencer_specific',
 }
 
 
-class ChannelEvent(MidiEvent):
-    def __init__(self, delta, event_type, data):
-        MidiEvent.__init__(self, delta, event_type & 0xf0, data)
-        self.channel = event_type & 0xf
+class MidiEvent:
+    """Represents an abstract event within a MIDI file."""
+    def __init__(self, **kwargs):
+        if kwargs.get("delta") is None:
+            raise Exception("A value for \"delta\" is required.")
+        if kwargs.get("type") is None:
+            raise Exception("A value for \"type\" is required.")
+        for k, v in kwargs.iteritems():
+            self.__dict__[k] = v
 
     def __repr__(self):
-        return MidiEvent.__repr__(self) + ", channel: {}".format(self.channel)
+        return str(self.__dict__)
 
-
-class NoteOffEvent(ChannelEvent):
-    def __init__(self, delta, event_type, data):
-        ChannelEvent.__init__(self, delta, event_type, data)
-        self.key = self.data[0]
-        self.velocity = self.data[1]
-
-    def __repr__(self):
-        return ChannelEvent.__repr__(self) + ", key: {}, velocity: {}".format(self.key, self.velocity)
-
-
-class NoteOnEvent(ChannelEvent):
-    def __init__(self, delta, event_type, data):
-        ChannelEvent.__init__(self, delta, event_type, data)
-        self.key = self.data[0]
-        self.velocity = self.data[1]
-
-    def __repr__(self):
-        return ChannelEvent.__repr__(self) + ", key: {}, velocity: {}".format(self.key, self.velocity)
-
-
-class PolyphonicKeyPressureEvent(ChannelEvent):
-    def __init__(self, delta, event_type, data):
-        ChannelEvent.__init__(self, delta, event_type, data)
-        self.key = self.data[0]
-        self.pressure = self.data[1]
-
-    def __repr__(self):
-        return ChannelEvent.__repr__(self) + ", key: {}, pressure: {}".format(self.key, self.pressure)
-
-
-class ControllerChangeEvent(ChannelEvent):
-    def __init__(self, delta, event_type, data):
-        ChannelEvent.__init__(self, delta, event_type, data)
-        self.controller = self.data[0]
-        self.value = self.data[1]
-
-    def __repr__(self):
-        return ChannelEvent.__repr__(self) + ", controller: {}, value: {}".format(self.controller, self.value)
-
-
-class ProgramChangeEvent(ChannelEvent):
-    def __init__(self, delta, event_type, data):
-        ChannelEvent.__init__(self, delta, event_type, data)
-        self.program = self.data[0]
-
-    def __repr__(self):
-        return ChannelEvent.__repr__(self) + ", program: {}".format(self.program)
-
-
-class ChannelKeyPressureEvent(ChannelEvent):
-    def __init__(self, delta, event_type, data):
-        ChannelEvent.__init__(self, delta, event_type, data)
-        self.pressure = self.data[0]
-
-    def __repr__(self):
-        return ChannelEvent.__repr__(self) + ", pressure: {}".format(self.pressure)
-
-
-class PitchBendEvent(ChannelEvent):
-    def __init__(self, delta, event_type, data):
-        ChannelEvent.__init__(self, delta, event_type, data)
-        self.amount = self.data[1] * 0x80 + self.data[0]
-
-    def __repr__(self):
-        return ChannelEvent.__repr__(self) + ", amount: {}".format(self.amount)
-
-
-class SysExEvent(MidiEvent):
-    def __init__(self, delta, event_type, data):
-        MidiEvent.__init__(self, delta, event_type, data)
-
-
-class MetaEvent(MidiEvent):
-    _META_TRANSLATE_MAP = {
-        0: ('Sequence Number', None),
-        1: ('Text Event', "text"),
-        2: ('Copyright', "text"),
-        3: ('Track Name', "text"),
-        4: ('Instrument Name', "text"),
-        5: ('Lyric', "text"),
-        6: ('Marker', "text"),
-        7: ('Cue Point', "text"),
-        0x20: ('Channel Prefix', None),
-        0x2f: ('End of Track', None),
-        0x51: ('Set Tempo', None),  # TODO Convert data to 3 byte tempo.
-        0x54: ('SMTPE Offset', None),  # TODO Convert data.
-        0x58: ('Time Signature', None),  # TODO Convert data.
-        0x59: ('Key Signature', None),  # TODO Convert data.
-        0x7f: ('Sequencer-Specific', None),  # TODO Convert data.
-    }
-
-    def __init__(self, delta, event_type, meta_type, data):
-        MidiEvent.__init__(self, delta, event_type, data)
-        # TODO Translate data into string/byte data depending on meta type.
-        translate_map = MetaEvent._META_TRANSLATE_MAP.get(meta_type)
-        if translate_map is None:
-            self.meta_type_name = "Unknown"
+    @classmethod
+    def create_event(cls, delta, event_type, stream):
+        args = {
+            "delta": delta
+        }
+        if event_type == F0_SYSEX_EVENT:
+            data_length = stream._read_var_length()
+            args.update({
+                "type": "f0_sysex",
+                "data": [stream._next_byte() for x in range(data_length)],
+            })
+        elif event_type == F7_SYSEX_EVENT:
+            data_length = stream._read_var_length()
+            args.update({
+                "type": "f7_sysex",
+                "data": [stream._next_byte() for x in range(data_length)],
+            })
+        elif event_type == META_EVENT:
+            args["type"] = "meta"
+            meta_type = stream._next_byte()
+            meta_type_name = _META_TYPE_NAMES.get(meta_type)
+            if meta_type_name is None:
+                meta_type_name = "unknown_0x{:x}".format((meta_type))
+            args["meta_type"] = meta_type_name
+            data_length = stream._read_var_length()
+            if meta_type_name in [
+                "text_event",
+                "copyright",
+                "track_name",
+                "instrument_name",
+                "lyric",
+                "marker",
+                "cue_point"]:
+                args["text"] = stream.f.read(data_length)
+            elif meta_type_name == "set_tempo":
+                args["speed"] = (stream._next_byte() << 16) + (stream._next_byte() << 8) + stream._next_byte()
+                args["bpm"] = 60000000 / args["speed"]  # 60 seconds as microseconds
+            # elif meta_type_name == "smtpe_offset":
+            #     # TODO Convert data.
+            #       hh mm ss fr     hours/minutes/seconds/frames in SMTPE format
+            #       ff 	            Fractional frame, in hundreth's of a frame
+            # elif meta_type_name == "time_signature":
+            #     # TODO Convert data.
+            #       nn/2^dd     eg: 6/8 would be specified using nn=6, dd=3
+            #       nn 	Time signature, numerator
+            #       dd 	Time signature, denominator expressed as a power of 2. eg a denominator of 4 is expressed as dd=2
+            #       cc 	MIDI Clocks per metronome tick
+            #       bb 	Number of 1/32 notes per 24 MIDI clocks (8 is standard)
+            # elif meta_type_name == "key_signature":
+            #     # TODO Convert data.
+            #       sf  number of sharps or flats, -7 = 7 flats, 0 = key of C, +7 = 7 sharps
+            #       mi  0 = major key, 1 = minor key
+            # elif meta_type_name == "sequencer_specific":
+            #     # TODO Convert data.
+            #       id      1 or 3 bytes representing the Manufacturer's ID
+            #       data    binary data
+            else:
+                if data_length:
+                    args["data"] = [stream._next_byte() for x in range(data_length)],
         else:
-            self.meta_type_name, data_type = translate_map
-            if data_type == "text":
-                self.data = struct.pack("B" * len(self.data), *self.data)
-        self.meta_type = meta_type
-
-    def __repr__(self):
-        return MidiEvent.__repr__(self) + ", type: {}, data: {}".format(self.meta_type_name, self.data)
+            args["channel"] = event_type & 0xf
+            event_type &= 0xf0
+            if event_type == NOTE_OFF_EVENT:
+                args.update({
+                    "type": "note_off",
+                    "key": stream._next_byte(),
+                    "velocity": stream._next_byte(),
+                })
+            elif event_type == NOTE_ON_EVENT:
+                args.update({
+                    "type": "note_on",
+                    "key": stream._next_byte(),
+                    "velocity": stream._next_byte(),
+                })
+            elif event_type == POLYPHONIC_KEY_PRESSURE_EVENT:
+                args.update({
+                    "type": "polyphonic_key_pressure",
+                    "key": stream._next_byte(),
+                    "pressure": stream._next_byte(),
+                })
+            elif event_type == CONTROLLER_CHANGE_EVENT:
+                args.update({
+                    "type": "controller_change",
+                    "controller": stream._next_byte(),
+                    "value": stream._next_byte(),
+                })
+            elif event_type == PROGRAM_CHANGE_EVENT:
+                args.update({
+                    "type": "program_change",
+                    "program": stream._next_byte(),
+                })
+            elif event_type == CHANNEL_KEY_PRESSURE_EVENT:
+                args.update({
+                    "type": "channel_key_pressure",
+                    "pressure": stream._next_byte(),
+                })
+            elif event_type == PITCH_BEND_EVENT:
+                args.update({
+                    "type": "pitch_bend",
+                    "pitch_bend": stream._next_byte() + stream._next_byte() * 0x80,
+                })
+            else:
+                raise Exception("Unsupported MIDI event code: 0x{:X}".format(event_type))
+        return MidiEvent(**args)
--- imfcreator/midi/reader.py
+++ imfcreator/midi/reader.py
@@ -1,7 +1,7 @@
 import os
 import struct
 from .constants import *
-from .events import MidiEvent, ChannelEvent
+from .events import MidiEvent
 
 
 class MidiReader:
@@ -17,27 +17,31 @@ class MidiReader:
     def load(self, filename):
         """Loads a MIDI file into the reader object."""
         with open(filename, "rb") as f:
-            self.f = f
-            # Start the chunk header generator. The data within the chunk must be handled separately.
-            chunk_headers = self._iterchunkheader()
-            chunk_name, chunk_length = chunk_headers.next()
-            if chunk_name != "MThd":
-                raise IOError("Not a MIDI file.")
-            if chunk_length != 6:
-                raise IOError("Invalid MIDI header length: {}".format(chunk_length))
-            self.file_format, num_tracks, self.division = struct.unpack(">HHH", f.read(6))
-            # if self.file_format not in (0, 1):
-            #     raise IOError("Unsupported MIDI file format: {}".format(self.file_format))
-            # print(self.file_format, num_tracks, self.division)
-            # Process remaining chunks.
-            for chunk_name, chunk_length in chunk_headers:
-                if chunk_name == "MTrk":
-                    track = MidiTrack()
-                    map(track.add_event, self._read_events(chunk_length))
-                    self.tracks.append(track)
-                else:
-                    # Skip any other chunk types that might appear.
-                    f.seek(chunk_length, os.SEEK_CUR)
+            try:
+                self.f = f
+                # Start the chunk header generator. The data within the chunk must be handled separately.
+                chunk_headers = self._iterchunkheader()
+                chunk_name, chunk_length = chunk_headers.next()
+                if chunk_name != "MThd":
+                    raise IOError("Not a MIDI file.")
+                if chunk_length != 6:
+                    raise IOError("Invalid MIDI header length: {}".format(chunk_length))
+                self.file_format, num_tracks, self.division = struct.unpack(">HHH", f.read(6))
+                # if self.file_format not in (0, 1):
+                #     raise IOError("Unsupported MIDI file format: {}".format(self.file_format))
+                # print(self.file_format, num_tracks, self.division)
+                # Process remaining chunks.
+                for chunk_name, chunk_length in chunk_headers:
+                    if chunk_name == "MTrk":
+                        track = MidiTrack()
+                        map(track.add_event, self._read_events(chunk_length))
+                        self.tracks.append(track)
+                        self._running_status = None
+                    else:
+                        # Skip any other chunk types that might appear.
+                        f.seek(chunk_length, os.SEEK_CUR)
+            finally:
+                self.f = None
 
     def _iterchunkheader(self):
         """A generator that assumes that the internal file object is positioned
@@ -81,36 +85,14 @@ class MidiReader:
             self._running_status = None
         # print("Event 0x{:x} at 0x{:x}".format(event_type, self.f.tell() - 1))
         event = MidiEvent.create_event(delta_time, event_type, self)
-        if isinstance(event, ChannelEvent):
-            self._running_status = event_type
-        print(event)
+        try:
+            if event.channel >= 0:
+                self._running_status = event_type
+        except AttributeError:
+            # Ignore attribute error.
+            pass
         return event
-        # meta_type = None
-        # event_data = None
-        # Read event data and return a MidiEvent object.
-        # if event_type in (F0_SYSEX_EVENT, F7_SYSEX_EVENT):
-        #     data_length = self._read_var_length()
-        # elif event_type == META_EVENT:
-        #     meta_type = self._next_byte()
-        #     data_length = self._read_var_length()
-        # elif event_type & 0xf0 in (
-        #         NOTE_OFF_EVENT,
-        #         NOTE_ON_EVENT,
-        #         POLYPHONIC_KEY_EVENT,
-        #         CONTROLLER_CHANGE_EVENT,
-        #         PITCH_BEND_EVENT):
-        #     self._running_status = event_type
-        #     data_length = 2
-        # elif event_type & 0xf0 in (
-        #         PROGRAM_CHANGE_EVENT,
-        #         CHANNEL_KEY_EVENT):
-        #     self._running_status = event_type
-        #     data_length = 1
-        # else:
-        #     raise IOError("Unsupported event type: 0x{:x} at 0x{:x}".format(event_type, self.f.tell() - 1) )
-        # if data_length > 0:
-        #     event_data = self.f.read(data_length)  # [f.next() for b in range(data_length)]
-        # return MidiEvent(delta_time, event_type, data_length, event_data, meta_type)
+
 
     def _read_var_length(self):
         """Reads a length using MIDI's variable length format."""
@@ -126,8 +108,11 @@ class MidiTrack:
     """Represents a MIDI track within a file and stores the events for the track."""
     def __init__(self):
         self._events = []
+        self.name = None
 
     def add_event(self, event):
+        if event.type == "meta" and event.meta_type == "track_name":
+            self.name = event.text
         self._events.append(event)
 
     @property
@@ -136,41 +121,3 @@ class MidiTrack:
 
     def __iter__(self):
         return self._events.__iter__()
-
-
-# class MidiEvent:
-#     """Represents an event within a MIDI file."""
-#     def __init__(self, delta, event_type, data_length, data, meta_type=None):
-#         self.delta = delta
-#         if NOTE_OFF_EVENT <= event_type < F0_SYSEX_EVENT:
-#             self.event_type = event_type & 0xf0
-#             self.channel = event_type & 0xf
-#         else:
-#             self.event_type = event_type
-#             self.channel = None
-#         self.data_length = data_length
-#         self.data = data
-#         if event_type == META_EVENT:
-#             self.meta_type = meta_type
-#
-#     def __repr__(self):
-#         return "Event 0x{:x}, channel: {}, delta: {}, data_length: {}".format(
-#             self.event_type,
-#             self.channel,
-#             self.delta,
-#             self.data_length)
-
-
-# #
-# # Helper functions
-# #
-# def read_uint32(f):
-#     return struct.unpack(">I", f.read(4))[0]
-#
-#
-# def read_uint16(f):
-#     return struct.unpack(">H", f.read(2))[0]
-#
-#
-# def read_byte(f):
-#     return struct.unpack("B", value)[0]
