8cf3e63b844b5091f5f164027e4ed77a9c9eced3
Wohlstand <admin@wohlnet.ru>
2019-11-13 15:49:48 +0300

    Save settings via shelve class

--- imfcreator/mainapplication.py
+++ imfcreator/mainapplication.py
@@ -1,4 +1,6 @@
 import sys
+import os
+import shelve
 from .filetypes.imfmusicfile import ImfMusicFile
 from player import ImfPlayer
 from resources import Resources
@@ -33,10 +35,11 @@ class MainApplication(tix.Tk):
         self.title('IMF Creator')
         # self.tk.call('wm', 'iconbitmap', self._w, '-default', os.path.join(Resources.PATH, 'icon.ico'))
         # self.settings = Settings()
+        self.settings = shelve.open('settings.dat', writeback=True)
         self.protocol("WM_DELETE_WINDOW", self._on_closing)
         self.player = ImfPlayer()
-        self.bank_file = "genmidi/GENMIDI.OP2"  # freedoom
-        self.song_path = "test/testtag.wlf"
+        self.bank_file = self.settings['bank_file'] if self.settings.has_key('bank_file') else "genmidi/GENMIDI.OP2"  # freedoom
+        self.song_path = self.settings['song_file'] if self.settings.has_key('song_file') else "test/testtag.wlf"
         self.instruments = None
         self.load_bank(self.bank_file)
         self.reload_song()
@@ -56,27 +59,33 @@ class MainApplication(tix.Tk):
         self.player.onstatechanged.add_handler(self.on_player_state_changed)
 
     def open_bank_file(self):
+        dir_path = os.path.dirname(self.bank_file) if self.bank_file is not None else None
         bank = tkFileDialog.askopenfilename(title="Open an instruments bank file",
                                             filetypes=(
                                                 ("Bank files", "*.op2 *.wopl *.OP2 *.WOPL"),
                                                 ("all files", "*.*")
                                             ),
-                                            parent=self)
+                                            parent=self,
+                                            initialdir=dir_path)
         if bank:
             self.bank_file = bank
+            self.settings['bank_file'] = self.bank_file
             self.load_bank(self.bank_file)
             self.reload_song()
 
     def open_music_file(self):
+        dir_path = os.path.dirname(self.song_path) if self.song_path is not None else None
         song = tkFileDialog.askopenfilename(title="Open a music file (MIDI or IMF)",
                                             filetypes=(
                                                 ("MIDI files", "*.mid *.MID"),
                                                 ("IMF files", "*.imf *.wlf *.IMF *.WLF"),
                                                 ("all files", "*.*")
                                             ),
-                                            parent=self)
+                                            parent=self,
+                                            initialdir=dir_path)
         if song:
             self.song_path = song
+            self.settings['song_file'] = self.song_path
             self.reload_song()
 
     def load_bank(self, path):
@@ -107,5 +116,7 @@ class MainApplication(tix.Tk):
         self.play_button.config(image=self.play_button.image)
 
     def _on_closing(self):
+        self.settings.sync()
+        self.settings.close()
         self.player.close()
         self.destroy()
