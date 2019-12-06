5e3d05928cec988346d6519b22bde57b68d85980
Adam Biser <adambiser@gmail.com>
2019-12-06 01:51:34 -0500

    Changed hanlding of instruments.
    There's now an instrument manager module that takes care of looading instrument files via plugins.  Multiple instrument files can be loaded and they'll be combined by the manager.
    
    InstrumentFile plugins are automatic now, no need to register them.
    Clarified InstrumentFile interface.
    
    General code cleaning.  PEP-8 standards, etc.

--- imfcreator/filetypes/op2fileplugin.py
+++ imfcreator/filetypes/op2fileplugin.py
@@ -1,56 +1,68 @@
-import instrumentfile
-from .instrumentfile import InstrumentFile
-from ..adlib import AdlibInstrument
 from ._binary import u8, u16le, s16le
+import imfcreator.adlib as _adlib
+import imfcreator.filetypes.instrumentfile as instrumentfile
+import imfcreator.instruments as _instruments
+import imfcreator.utils as _utils
 
 
-def _accept(preview):
-    return preview[0:8] == "#OPL_II#"
+class Op2FilePlugin(instrumentfile.InstrumentFile):
+    """Read instruments from a DMX Sound Library file."""
+    DESCRIPTION = "DMX Sound Library"
+    _FILE_SIGNATURE = b"#OPL_II#"
+    _FILE_SIZE = 11908
+    _ENTRY_START = 8
+    _ENTRY_SIZE = 36
+    _FIRST_PERCUSSION_ENTRY = 128
+    _FIRST_PERCUSSION_PROGRAM = 35
+    _ENTRY_COUNT = 175  # Always 175 for OP2 files.
+    _NAME_START = _ENTRY_START + _ENTRY_SIZE * _ENTRY_COUNT
+    _NAME_SIZE = 32
+    _FLAG_USE_GIVEN_NOTE = 1
+    _FLAG_UNKNOWN = 2
+    _FLAG_USE_SECONDARY_VOICE = 4
 
-
-class Op2FilePlugin(InstrumentFile):
-    ID = "OP2"
-    DESCRIPTION = "OPL2 DMX Sound Library"
-    ENTRY_START = 8
-    ENTRY_SIZE = 36
-    NUM_ENTRIES = 175  # Always 175 for OP2 files.
-    NAME_START = ENTRY_START + ENTRY_SIZE * NUM_ENTRIES
-    NAME_SIZE = 32
-    FLAG_USE_GIVEN_NOTE = 1
-    FLAG_UNKNOWN = 2
-    FLAG_USE_SECONDARY_VOICE = 4
-    # FLAG_USE_FINE_TUNING = 4???
+    @classmethod
+    def accept(cls, preview: bytes):
+        return preview[0:8] == Op2FilePlugin._FILE_SIGNATURE
 
     def _open(self):
-        self.seek(0)
+        if _utils.get_file_size(self.fp) != Op2FilePlugin._FILE_SIZE:
+            raise ValueError("Unexpected file size.")
+        if not Op2FilePlugin.accept(self.fp.read(8)):
+            raise ValueError("Unexpected file signature.")
 
-    def seek(self, index):
-        self.info = [
-            Op2FilePlugin.ENTRY_START + index * Op2FilePlugin.ENTRY_SIZE,
-            Op2FilePlugin.NAME_START + index * Op2FilePlugin.NAME_SIZE,
-        ]
+    @property
+    def instrument_count(self) -> int:
+        return Op2FilePlugin._ENTRY_COUNT
 
-    def load(self):
-        entry_offset, name_offset = self.info
-        self.info = []
+    def get_instrument(self, index: int) -> (int, int, _adlib.AdlibInstrument, int):
+        entry_offset = Op2FilePlugin._ENTRY_START + index * Op2FilePlugin._ENTRY_SIZE
+        name_offset = Op2FilePlugin._NAME_START + index * Op2FilePlugin._NAME_SIZE
         # Read entry and name from file.
         self.fp.seek(entry_offset)
-        entry = self.fp.read(Op2FilePlugin.ENTRY_SIZE)
+        entry = self.fp.read(Op2FilePlugin._ENTRY_SIZE)
         self.fp.seek(name_offset)
-        name = self.fp.read(Op2FilePlugin.NAME_SIZE).partition('\x00')[0]
+        name = self.fp.read(Op2FilePlugin._NAME_SIZE).partition(b'\x00')[0]
         # Set up the instrument.
-        instrument = AdlibInstrument(name=name, num_voices=2)
+        instrument = _adlib.AdlibInstrument(name=name, num_voices=2)
         flags = u16le(entry[0:2])
-        instrument.use_secondary_voice = flags & Op2FilePlugin.FLAG_USE_SECONDARY_VOICE
-        instrument.use_given_note = flags & Op2FilePlugin.FLAG_USE_GIVEN_NOTE
+        instrument.use_secondary_voice = flags & Op2FilePlugin._FLAG_USE_SECONDARY_VOICE
+        instrument.use_given_note = flags & Op2FilePlugin._FLAG_USE_GIVEN_NOTE
         instrument.fine_tuning = u8(entry[2])
         instrument.given_note = u8(entry[3])
         Op2FilePlugin._read_voice(instrument, 0, entry[4:20])
         Op2FilePlugin._read_voice(instrument, 1, entry[20:36])
-        return instrument
+        if index < Op2FilePlugin._FIRST_PERCUSSION_ENTRY:
+            inst_type = _instruments.MELODIC
+            program = index
+        else:
+            inst_type = _instruments.PERCUSSION
+            program = index - Op2FilePlugin._FIRST_PERCUSSION_ENTRY + Op2FilePlugin._FIRST_PERCUSSION_PROGRAM
+        return inst_type, program, instrument
 
     @staticmethod
     def _read_voice(instrument, voice_number, data):
+        """Read voice data."""
         instrument.modulator[voice_number].set_regs(**Op2FilePlugin._get_operator_regs(data[0:6]))
         instrument.feedback[voice_number] = u8(data[6])
         instrument.carrier[voice_number].set_regs(**Op2FilePlugin._get_operator_regs(data[7:13]))
@@ -58,7 +70,8 @@ class Op2FilePlugin(InstrumentFile):
         instrument.note_offset[voice_number] = s16le(data[14:16])
 
     @staticmethod
-    def _get_operator_regs(data):
+    def _get_operator_regs(data) -> dict:
+        """Get the register values from the given data."""
         return {
             "tvskm": u8(data[0]),
             "attack_decay": u8(data[1]),
@@ -66,11 +79,3 @@ class Op2FilePlugin(InstrumentFile):
             "waveform_select": u8(data[3]),
             "ksl_output": u8(data[4]) | u8(data[5])
         }
-
-    @property
-    def num_instruments(self):
-        return Op2FilePlugin.NUM_ENTRIES
-
-
-# Register the plugin.
-instrumentfile.register_open(Op2FilePlugin.ID, Op2FilePlugin, _accept)
