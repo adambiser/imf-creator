735e18ff4feef3a433e417eb6565d54490bf4943
Adam Biser <adambiser@gmail.com>
2019-12-10 18:24:11 -0500

    Made struct import private.

--- imfcreator/plugins/_binary.py
+++ imfcreator/plugins/_binary.py
@@ -1,59 +1,59 @@
 """Utility methods for unpacking byte data."""
-import struct
+import struct as _struct
 
 
 def u8(c):
     """Read an unsigned 8-bit integer."""
     try:
-        return struct.unpack("<B", c)[0]
+        return _struct.unpack("<B", c)[0]
     except TypeError:
-        return struct.unpack("<B", bytes([c]))[0]
+        return _struct.unpack("<B", bytes([c]))[0]
 
 
 def s8(c):
     """Read a signed 8-bit integer."""
     try:
-        return struct.unpack("<b", c)[0]
+        return _struct.unpack("<b", c)[0]
     except TypeError:
-        return struct.unpack("<b", bytes([c]))[0]
+        return _struct.unpack("<b", bytes([c]))[0]
 
 
 def u16le(c):
     """Read an unsigned little-endian 16-bit integer."""
-    return struct.unpack("<H", c)[0]
+    return _struct.unpack("<H", c)[0]
 
 
 def u16be(c):
     """Read an unsigned big-endian 16-bit integer."""
-    return struct.unpack(">H", c)[0]
+    return _struct.unpack(">H", c)[0]
 
 
 def s16le(c):
     """Read a signed little-endian 16-bit integer."""
-    return struct.unpack("<h", c)[0]
+    return _struct.unpack("<h", c)[0]
 
 
 def s16be(c):
     """Read a signed big-endian 16-bit integer."""
-    return struct.unpack(">h", c)[0]
+    return _struct.unpack(">h", c)[0]
 
 
 def u32le(c):
     """Read an unsigned little-endian 32-bit integer."""
-    return struct.unpack("<I", c)[0]
+    return _struct.unpack("<I", c)[0]
 
 
 def u32be(c):
     """Read an unsigned big-endian 32-bit integer."""
-    return struct.unpack(">I", c)[0]
+    return _struct.unpack(">I", c)[0]
 
 
 def s32le(c):
     """Read a signed little-endian 32-bit integer."""
-    return struct.unpack("<i", c)[0]
+    return _struct.unpack("<i", c)[0]
 
 
 def s32be(c):
     """Read a signed big-endian 32-bit integer."""
-    return struct.unpack(">i", c)[0]
+    return _struct.unpack(">i", c)[0]
 
