0603c457c8463265d8d390cb2bb18e81f11631e3
Adam Biser <adambiser@gmail.com>
2021-03-23 22:36:43 -0400

    Restructure tests folder

--- tests/test_midisong.py
+++ tests/test_midisong.py
@@ -1,52 +1,136 @@
-from shutil import copyfile
+import inspect
 import logging
+import io
 import os
 import unittest
 
-import imfcreator.plugins
+try:
+    # Only show errors when importing here.
+    logging.basicConfig(level=logging.ERROR, format='%(levelname)s\t%(message)s')
+    import imfcreator.plugins
+    import imfcreator.instruments
+except ImportError:
+    raise
 
-imfcreator.plugins.load_plugins()
-
-_INPUT_FOLDER = os.path.abspath("input")
-_OUTPUT_FOLDER = os.path.abspath("output")
-_VERIFY_FOLDER = os.path.abspath("verify")
-
-os.makedirs(_OUTPUT_FOLDER, exist_ok=True)
+_FILES_FOLDER = os.path.abspath("files")
+_MIDI_FILES = [
+    "AC.mid",
+    "ievan_polkka2.mid",
+]
 
 
-class TestMidiSong(unittest.TestCase):
-    INPUT_FILES = ["AC.mid"]
-    TEMP_FILENAME = "output.txt"
+class LoggingTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.stream = io.StringIO()
+        self.handler = logging.StreamHandler(self.stream)
+        self.handler.setLevel(logging.WARNING)
+        self.handler.setFormatter(logging.Formatter('%(levelname)s\t%(message)s'))
+        self.logger = logging.getLogger('')
+        for handler in self.logger.handlers:
+            self.logger.removeHandler(handler)
+        self.logger.addHandler(self.handler)
 
-    # def setUp(self) -> None:
-    #     logging.basicConfig(level=logging.DEBUG, format='%(levelname)s\t%(message)s',
-    #                         filename=TestMidiSong.TEMP_FILENAME)
-    #
-    # def tearDown(self) -> None:
-    #     if os.path.exists(TestMidiSong.TEMP_FILENAME):
-    #         os.remove(TestMidiSong.TEMP_FILENAME)
+    def tearDown(self) -> None:
+        self.logger.removeHandler(self.handler)
+        self.handler.close()
 
-    def test_file(self):
-        filename = os.path.join(_INPUT_FOLDER, TestMidiSong.INPUT_FILES.pop(0))
-        song = imfcreator.plugins.MidiSongFile.load_file(filename)
-        lines = song.get_debug_info()
-        dump_filename = os.path.basename(filename) + ".txt"
-        output_filename = os.path.join(_OUTPUT_FOLDER, dump_filename)
-        with open(output_filename, "w") as fp:
-            fp.writelines(line + "\n" for line in lines)
-        # Verify contents.
-        verify_filename = os.path.join(_VERIFY_FOLDER, dump_filename)
-        if os.path.exists(verify_filename):
-            with open(verify_filename, "r") as fp:
-                check = fp.read().splitlines()
-            self.assertEqual(len(lines), len(check), "Line count mismatch.")
-            for index in range(len(lines)):
-                self.assertEqual(lines[index], check[index], f"Line {index} differs.")
+    def get_stream_value(self):
+        self.handler.flush()
+        return self.stream.getvalue()
+
+
+class PluginTestCase(LoggingTestCase):
+    def test_load_plugins(self):
+        # noinspection PyBroadException
+        try:
+            imfcreator.plugins.load_plugins()
+        except Exception:
+            self.fail("Exception loading plugins")
+
+    def test_check_for_midi_filetype(self):
+        filetypes = imfcreator.plugins.MidiSongFile.get_filetypes()
+        value = next((ft for ft in filetypes if ft.default_extension == "mid"), None)
+        self.assertIsNotNone(value, "Could not find plugin for 'mid' file type.")
+
+    def test_check_for_imf1_filetype(self):
+        value = imfcreator.plugins.AdlibSongFile.get_filetype_class("imf1")
+        self.assertIsNotNone(value, "Could not find plugin for 'imf1' file type.")
+
+
+class InstrumentTestCase(LoggingTestCase):
+    def test_load_wopl(self):
+        # imfcreator.instruments.add_file(os.path.join(_FILES_FOLDER, "GENMIDI.GS.wopl"))
+        # self.assertEqual(imfcreator.instruments.count(), 330)
+        imfcreator.instruments.add_file(os.path.join(_FILES_FOLDER, "Apogee-IMF-90.wopl"))
+        self.assertEqual(imfcreator.instruments.count(), 174)
+
+
+class SongTestCase(LoggingTestCase):
+    def __init__(self, method_name, filename):
+        super().__init__(methodName=method_name)
+        self.filename = filename
+
+    def compare_to_results(self, debug_info: str):
+        calling_function = inspect.stack()[1].function
+        result_file = self.filename + f".{calling_function}.log"
+        if os.path.exists(result_file):
+            try:
+                with open(result_file, "r") as fp:
+                    result_info = fp.read().splitlines()
+                debug_lines = debug_info.splitlines()
+                self.assertEqual(len(debug_lines), len(result_info), "Line count mismatch.")
+                for index in range(len(debug_lines)):
+                    self.assertEqual(debug_lines[index], result_info[index],
+                                     f"First difference found in line {index + 1}.  "
+                                     f"File: {os.path.basename(result_file)}")
+            except AssertionError:
+                # Write the new results to a temp file.
+                with open(self.filename + f".{calling_function}.fail.log", "w") as fp:
+                    fp.write(debug_info)
+                raise
         else:
             # No verification, save what we have for next time.
-            copyfile(output_filename, verify_filename)
-            self.assertFalse(False, f"Copied verify file: {verify_filename}")
+            with open(result_file, "w") as fp:
+                fp.write(debug_info)
+            self.fail(f"Verify file did not exist.  Created verify file: {os.path.basename(result_file)}")
+
+    def test_midi_load(self):
+        midi_song = imfcreator.plugins.MidiSongFile.load_file(self.filename)
+        debug_info = midi_song.get_debug_info()
+        self.compare_to_results(debug_info)
+
+    def test_convert_to_imf(self):
+        midi_song = imfcreator.plugins.MidiSongFile.load_file(self.filename)
+        logging.basicConfig(level=logging.DEBUG, format='%(levelname)s\t%(message)s')
+        adlib_song = imfcreator.plugins.AdlibSongFile.convert_from(midi_song, "imf1")
+        debug_info = adlib_song.get_debug_info()
+        self.compare_to_results(debug_info)
+
+
+def get_test_files(path: str, extension: str):
+    """Extension should include the period and is case insensitive."""
+    extension = extension.lower()
+    return [os.path.join(root, f)
+            for root, _, files in os.walk(path)
+            for f in files
+            if os.path.splitext(f)[1].lower() == extension]
+
+
+def load_tests():
+    suite = unittest.TestSuite([
+        PluginTestCase("test_load_plugins"),
+        PluginTestCase("test_check_for_midi_filetype"),
+        PluginTestCase("test_check_for_imf1_filetype"),
+        InstrumentTestCase("test_load_wopl"),
+    ])
+    # for filename in get_test_files(_FILES_FOLDER, ".mid"):
+    for f in _MIDI_FILES:
+        filename = os.path.join(_FILES_FOLDER, f)
+        suite.addTest(SongTestCase("test_midi_load", filename))
+        suite.addTest(SongTestCase("test_convert_to_imf", filename))
+    return suite
 
 
 if __name__ == "__main__":
-    unittest.main()
+    runner = unittest.TextTestRunner(failfast=True)
+    runner.run(load_tests())
