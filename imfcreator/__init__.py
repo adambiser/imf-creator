709573eb0a44ae783258781796f1b354619a5490
Adam Biser <adambiser@gmail.com>
2021-03-26 16:31:22 -0400

    Default to INFO messages

--- imfcreator/__init__.py
+++ imfcreator/__init__.py
@@ -1,4 +1,4 @@
 import logging
 
 # TODO Move into a config.py module?
-logging.basicConfig(level=logging.DEBUG, format='%(levelname)s\t%(message)s')
+logging.basicConfig(level=logging.INFO, format='%(levelname)s\t%(message)s')
